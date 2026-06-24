0dc5e3462e116f22b0c722f1e5d5af271b54d7ef
unknown <vtu29179@veltech.edu.in>
2026-06-24 17:15:22 +0530

    Initial SmartShelf project

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+
+# Database
+db = client["smartshelf"]
+
+# Collection
+products = db["products"]
+
+# Home Page
+@app.route('/')
+def home():
+
+    data = list(products.find())
+
+    for p in data:
+
+        if p["stock"] <= p["threshold"]:
+            p["alert"] = "⚠️ Low Stock"
+        else:
+            p["alert"] = "✅ Available"
+
+    return render_template("index.html", data=data)
+
+# Add Product
+@app.route('/add', methods=['POST'])
+def add():
+
+    product = {
+        "name": request.form['name'],
+        "category": request.form['category'],
+        "stock": int(request.form['stock']),
+        "threshold": int(request.form['threshold']),
+        "daily_usage": int(request.form['daily_usage'])
+    }
+
+    products.insert_one(product)
+
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
